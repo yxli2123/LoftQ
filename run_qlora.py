b806ae5997221979d1d4016c89a4f7d7937c4cd8
Yixiao Li, Macbook Air <yxli2123@gmail.com>
2023-10-14 00:09:29 -0400

    try float16

--- run_qlora.py
+++ run_qlora.py
@@ -32,7 +32,7 @@ def main():
             quantization_config=BitsAndBytesConfig(
                 load_in_4bit=True,
                 llm_int8_has_fp16_weight=False,
-                bnb_4bit_compute_dtype=torch.float32,
+                bnb_4bit_compute_dtype=torch.float16,
                 bnb_4bit_use_double_quant=False,
                 bnb_4bit_quant_type='nf4',
             ),
