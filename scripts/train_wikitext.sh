d639d73ffa7fc3312635f0c6bd7cef2fc19168ba
Yixiao Li, Macbook Air <yxli2123@gmail.com>
2023-12-27 09:58:09 -0500

    update train clm

--- scripts/train_wikitext.sh
+++ scripts/train_wikitext.sh
@@ -1,41 +1,46 @@
-# train 2-bit 64-rank llama-2-7b on wikitext-2 using 1 GPU
+# train 4-bit 64-rank llama-2-7b on wikitext-2 using 1 GPU
 python train_clm.py \
---model_name_or_path LoftQ/Llama-2-7b-hf-bit4-rank64 \
---output_dir exp_results/wikitext-2/bit4-rank64_ft \
---learning_rate 1e-4  \
---seed 888 \
+--full_precision \
+--model_name_or_path LoftQ/Llama-2-7b-hf-4bit-64rank \
+--output_dir exp_results/wikitext-2/ \
+--learning_rate 3e-4  \
+--seed 11 \
 --dataset_name wikitext \
 --dataset_config wikitext-2-raw-v1 \
---num_train_epochs 2 \
---per_device_train_batch_size 1 \
---per_device_eval_batch_size 1 \
---gradient_accumulation_steps 8 \
---do_train \
---do_eval \
+--num_train_epochs 3 \
+--per_device_train_batch_size 4 \
+--per_device_eval_batch_size 4 \
+--gradient_accumulation_steps 16 \
+--save_strategy "epoch" \
+--weight_decay 0.1 \
+--warmup_ratio 0.03 \
+--lr_scheduler_type "cosine" \
+--logging_steps 1 \
+--do_train --do_eval \
 --logging_steps 50 \
---evaluation_strategy epoch \
 --report_to tensorboard \
---overwrite_output_dir \
---block_size 1024 \
+--block_size 1024
 
 
-# train 2-bit 64-rank llama-2-7b on wikitext-2 using multiple GPUs
+# train 4-bit 64-rank llama-2-7b on wikitext-2 using 8 GPUs
 accelerate launch train_clm.py \
---fake_quantization \
---model_name_or_path LoftQ/Llama-2-7b-hf-bit4-rank64 \
---output_dir exp_results/wikitext-2/bit4-rank64_ft \
---learning_rate 1e-4  \
---seed 888 \
+--full_precision \
+--model_name_or_path LoftQ/Llama-2-7b-hf-4bit-64rank \
+--output_dir exp_results/wikitext-2/ \
+--learning_rate 3e-4  \
+--seed 11 \
 --dataset_name wikitext \
 --dataset_config wikitext-2-raw-v1 \
---num_train_epochs 2 \
---per_device_train_batch_size 1 \
---per_device_eval_batch_size 1 \
---gradient_accumulation_steps 8 \
---do_train \
---do_eval \
+--num_train_epochs 3 \
+--per_device_train_batch_size 2 \
+--per_device_eval_batch_size 4 \
+--gradient_accumulation_steps 4 \
+--save_strategy "epoch" \
+--weight_decay 0.1 \
+--warmup_ratio 0.03 \
+--lr_scheduler_type "cosine" \
+--logging_steps 1 \
+--do_train --do_eval \
 --logging_steps 50 \
---evaluation_strategy epoch \
 --report_to tensorboard \
---overwrite_output_dir \
 --block_size 1024
